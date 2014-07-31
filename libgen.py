298c13ec28927cdf84038da30e2897668ef3e5d0
anomico <code@anomi.co>
2014-07-31 21:41:04 +0100

    Refactoring download function

--- libgen.py
+++ libgen.py
@@ -6,9 +6,13 @@ A short and sweet script to download books from libgen.org."""
 
 import re
 import sys
+import copy
 import math
 import urllib2
 import argparse
+import tabulate
+import itertools
+import multiprocessing
 from bs4 import BeautifulSoup
 
 searchurl = 'http://libgen.org/search.php?&req=%s&view=simple&column=def&sort=title&sortmode=ASC&page=%d'
@@ -44,22 +48,39 @@ def search(term):
     for page in _next_page(term, numberofbooks):
         yield BeautifulSoup(urllib2.urlopen(page))
 
-# def extract(page):
-#     """Return a list of md5 hashes corresponding to each book found on a single search page."""
-#     data = BeautifulSoup(page)
-#     print(data.find(text=re.compile("^\d+ books found"))
-#     # data.find_all(href=re.compile("book/index.php\?md5="))
+def extract(page):
+    """Extract all the books info in a given result page.
+
+    :param page: result page as an BeautifulSoup4 object
+    :returns: list of books as a list of dicts
+    """
+    # data.find_all(href=re.compile("book/index.php\?md5="))
+    # for i, data in enumerate():
+    # book['id'] = i
+    # book['author'] = None
+    # book['title'] = None
+    # book['publisher'] = None
+    # book['year'] = None
+    # book['pages'] = None
+    # book['lang'] = None
+    # book['size'] = None
+    # book['extension'] = None
+    # book['hash'] = None
+    pass
 
 def select(books):
     """
-    Print the books on a single search result page and
-    allows the user to choose one to download.
+    Print the books info on a single search result page
+    and allows the user to choose one to download.
 
     :param books: list of books
+    :returns: a book as a dict or None if not found
     """
-    for book in books:
-        print('{0}. {1} {2} {3}'.format(book['id'], book['author'], book['title'], book['publisher'])),
-        print('{0} {1} {2} {3} {4}'.format(book['year'], book['pages'], book['lang'], book['size'], book['extension']))
+    headers = ['ID', 'Author(s)', 'Title', 'Publisher', 'Year', 'Pages', 'Language', 'Size', 'Extension']
+    print(tabulate.tabulate([b.values() for b in books], headers))
+    # for book in books:
+    #    print('ID:{0} {1} - {2} {3}'.format(book['id'], book['author'], book['title'], book['publisher'])),
+    #    print('{0} {1} {2} {3} {4}'.format(book['year'], book['pages'], book['lang'], book['size'], book['extension']))
     while True:
         try:
             choice = int(raw_input('Choose book: '))
@@ -68,7 +89,11 @@ def select(books):
         except ValueError: print('Invalid choice. Try again.'); continue
         except (KeyboardInterrupt, EOFError): print(''); sys.exit(0)
         break
-    return next((b for b in books if b['id'] == choice), None)
+    book = next((b for b in books if b['id'] == choice), None)
+    if not book:
+        print('No book with this ID.')
+        sys.exit(1)
+    return book
 
 def download(book):
     """
@@ -76,51 +101,30 @@ def download(book):
 
     :param book: md5 hash of a book
     """
-    r = urllib2.urlopen(downloadurl + book)
-    filename = re.search('filename=\"(.+)\"', r.info()['Content-Disposition']).group(1)
-    with open(filename, 'wb') as f:
-        filesize = int(r.info().getheaders("Content-Length")[0])
-        print("Downloading: %s\nSize: %.2f MB" % (' - '.join(filename.split('-')[:2]), float(filesize) / 10**6))
+    blocksize = 1024 # in bytes
+    filename = book['title'] + '.' + book['extension']
+    bookurl = downloadurl + book['hash']
+    filesize = int(urllib2.urlopen(bookurl).info().getheaders("Content-Length")[0]) # in bytes
+    req = urllib2.Request(bookurl)
+    parts = _range(0, filesize, blocksize)
+    req = urllib2.Request(downloadurl + book['hash'])
+    requests = list(itertools.repeat(req, len(parts))) # make len(parts) request copies
+    reqs = []
+    for r, rng in itertools.izip(requests, parts):
+        reqs.append(r.add_header('Range', 'bytes={0}-{1}'.format(rng[0], rng[1])))
+    for r in reqs: print(r.headers)
+    return
+    #requests = [r.add_header('Range', 'bytes={0}-{1}'.format(rng[0], rng[1])) for r, rng in itertools.izip(requests, parts)] # change headers
 
-        filesize_dl = 0
-        block_sz = 1024
-        while True:
-            buf = r.read(block_sz)
-            if not buffer:
-                break
-            filesize_dl += len(buf)
-            f.write(buf)
-            status = r"%10d  [%3.2f%%]" % (filesize_dl, filesize_dl * 100. / filesize)
-            status = status + chr(8)*(len(status)+1)
-            print status,
+    def _download_and_save(req):
+        with open(filename, 'wb') as f:
+            f.seek(byte)
+            f.write(req.get_data())
 
-def download2(book):
-    """
-    Download a book from libgen.org to the current directory.
+    p = multiprocessing.Pool(processes=3)
+    parts = p.map(_download_and_save, requests)
 
-    :param book: a md5 hash of a book
-    """
-    blocksize = 1024 # bytes
-    req = urllib2.Request(downloadurl + book)
-    req.headers['Range'] = 'bytes=%s-%s' % (start, end)
-    parts = list(_parts('bytes=%s-%s', _range(0, filesize, blocksize)))
-    r = urllib2.urlopen(req)
-    filename = re.search('filename=\"(.+)\"', r.info()['Content-Disposition']).group(1)
-    with open(filename, 'wb') as f:
-        filesize = int(r.info().getheaders("Content-Length")[0])
-        print("Downloading: %s\nSize: %.2f MB" % (' - '.join(filename.split('-')[:2]), float(filesize) / 10**6))
-
-        filesize_dl = 0
-        while True:
-            buf = r.read(blocksize)
-            if not buffer:
-                break
-            filesize_dl += len(buf)
-            f.write(buf)
-            status = r"%10d  [%3.2f%%]" % (filesize_dl, filesize_dl * 100. / filesize)
-            status = status + chr(8)*(len(status)+1)
-            print status,
-
+    # filename = re.search('filename=\"(.+)\"', r.info()['Content-Disposition']).group(1)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Read more, kids.')
@@ -129,14 +133,13 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--type', dest='extension', default='pdf', help='file extension')
     args = parser.parse_args()
 
-    # make search function a generator so that we can do this:
-    # for result_page in search(args.search):
-    #     extract(result_page)     
-    #     reduce(extractaccumulate, search(options.search))
-    #     reduce(lambda a, b: extract(a) + extract(b), search(options.search)) 
-    # extract(search(sys.argv[1]))
-    download2("6738829E0C619C853DFE3507C80BCE98")
-    #for page in search(args.search):
-    #    print page.prettify()
+    """
+    for result_page in search(args.search):
+        selected = select(extract(result_page))
+        if selected:
+            download(selected['hash']
+    """
+
+    download("6738829E0C619C853DFE3507C80BCE98")
 
     # 'Downloaded X MB in Y seconds.'
