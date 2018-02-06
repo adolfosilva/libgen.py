d2f7fce3cac7b2d742ab553325b3394092a0c8f8
Adolfo Silva <code@adolfosilva.org>
2018-02-06 09:11:53 +0000

    Change the name of the package
    Full length 'version'.
    Add 'classifiers' and 'keywords'.
    Include files in MANIFEST.in in the dist.

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
 
 setup(
-    name='libgen',
-    version='0.1',
+    name='libgen.py',
+    version='0.1.0',
     license='MIT',
     author='Adolfo Silva',
     author_email='code@adolfosilva.org',
     url='https://github.com/adolfosilva/libgen.py',
     description='A script to download books from gen.lib.rus.ec',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='libgen',
+    include_package_data=True,  # include files listed in MANIFEST.in
     tests_requires=['pytest'],
     py_modules=['libgen'],
     entry_points={
