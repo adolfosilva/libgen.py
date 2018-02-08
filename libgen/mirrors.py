4785e28748440168e38d08a94605a7fa2ba78d13
Adolfo Silva <code@adolfosilva.org>
2018-02-08 12:44:38 +0000

    Change 'download_book' to 'download_publication'
    Remove unused imports.
    Extract static method 'Mirror.get_href'.
    Print stacktrace on catching Exception.
    Extract publication info extraction functionality
    to 'extract_attributes'.

--- libgen/mirrors.py
+++ libgen/mirrors.py
@@ -7,7 +7,7 @@ from abc import ABC
 import re
 import sys
 import itertools
-from typing import Generator, List
+from typing import Generator, List, Dict, Any
 
 import requests
 from requests.exceptions import Timeout
@@ -19,6 +19,11 @@ from bs4 import BeautifulSoup
 class Mirror(ABC):
     def __init__(self, search_url: str) -> None:
         self.search_url = search_url
+
+    @staticmethod
+    def get_href(cell):
+        # TODO: safe [0] please
+        return cell.findAll('a', href=True)[0]['href']
 
     def run(self):
         try:
@@ -103,14 +108,16 @@ class Mirror(ABC):
         for (n, mirror) in publication.mirrors.items():
             # print(f"About to try {n}\n")
             try:
-                mirror.download_book(publication)
+                mirror.download_publication(publication)
                 break  # stop if successful
             except (CouldntFindDownloadUrl, Timeout) as e:
                 print(e)
                 print("Trying a different mirror.")
                 continue
             except Exception:
-                print("An error occurred.")
+                import traceback
+                print(f"An error occurred: {sys.exc_info()[0]}")
+                print(traceback.format_exc())
                 print("Trying a different mirror.")
                 continue
         print("Failed to download publications.")
@@ -129,45 +136,49 @@ class GenLibRusEc(Mirror):
             yield f"{self.search_url}{self.search_term}&page={str(pn)}"
 
     def extract(self, page):
-        """Extract all the books info in a given result page.
+        """Extract all the publications info in a given result page.
 
         :param page: result page as an BeautifulSoup4 object
         :returns: list of Publication
         """
-        r = re.compile("(.+)(\[(.+)\])?(.*)")
-        trs = page.findAll('table')[2].findAll('tr')
+        rows = page.findAll('table')[2].findAll('tr')
         results = []
-        for tr in trs[1:]:
-            td = tr.findAll('td')
-            attrs = {}
-            attrs['id'] = td[0].text
-            attrs['authors'] = td[1].text.strip()
-            t = r.search(td[2].text.strip())
-            if t is None:
-                attrs['title'] = td[2].text.strip()
-            else:
-                attrs['title'] = t.group(1).strip()
-                attrs['edition'] = t.group(2)
-                attrs['isbn'] = t.group(3)
-            attrs['publisher'] = td[3].text
-            attrs['year'] = td[4].text
-            attrs['pages'] = td[5].text
-            attrs['lang'] = td[6].text
-            attrs['size'] = td[7].text
-            attrs['extension'] = td[8].text
-            # TODO: refactor (eliminate duplication - td[x]...)
-            libgen_io_url = td[9].findAll('a', href=True)[0]['href']
-            libgen_pw_url = td[10].findAll('a', href=True)[0]['href']
-            # bok_org_url = td[11].findAll('a', href=True)[0]['href']
-            # bookfi_net_url = td[12].findAll('a', href=True)[0]['href']
-            attrs['mirrors'] = {
-                    'libgen.io': downloaders.LibgenIoDownloader(libgen_io_url),
-                    'libgen.pw': downloaders.LibgenPwDownloader(libgen_pw_url),
-                    # 'b-ok.org': downloaders.BOkOrgDownloader(bok_org_url),
-                    # 'bookfi.net': downloaders.BookFiNetDownloader(bookfi_net_url)
-            }
+        for row in rows[1:]:
+            cells = row.findAll('td')
+            attrs = self.extract_attributes(cells)
             results.append(Publication(attrs))
         return results
+
+    def extract_attributes(self, cells) -> Dict[str, Any]:
+        r = re.compile("(.+)(\[(.+)\])?(.*)")
+        attrs = {}
+        attrs['id'] = cells[0].text
+        attrs['authors'] = cells[1].text.strip()
+        t = r.search(cells[2].text.strip())
+        if t is None:
+            attrs['title'] = cells[2].text.strip()
+        else:
+            attrs['title'] = t.group(1).strip()
+            attrs['edition'] = t.group(2)
+            attrs['isbn'] = t.group(3)
+        attrs['publisher'] = cells[3].text
+        attrs['year'] = cells[4].text
+        attrs['pages'] = cells[5].text
+        attrs['lang'] = cells[6].text
+        attrs['size'] = cells[7].text
+        attrs['extension'] = cells[8].text
+        # TODO: refactor (eliminate duplication - cells[x]...)
+        libgen_io_url = Mirror.get_href(cells[9])
+        libgen_pw_url = Mirror.get_href(cells[10])
+        # bok_org_url = Mirror.get_href(cells[11])
+        # bookfi_net_url = Mirror.get_href(cells[12])
+        attrs['mirrors'] = {
+                'libgen.io': downloaders.LibgenIoDownloader(libgen_io_url),
+                'libgen.pw': downloaders.LibgenPwDownloader(libgen_pw_url),
+                # 'b-ok.org': downloaders.BOkOrgDownloader(bok_org_url),
+                # 'bookfi.net': downloaders.BookFiNetDownloader(bookfi_net_url)
+        }
+        return attrs
 
 
 class LibGenPw(Mirror):
