d67706b4eb01bf2a8123698e5b30884aa93e052d
Adolfo Silva <code@adolfosilva.org>
2018-02-08 12:13:19 +0000

    Add tests for '__len__' and 'attributes' methods

--- tests/test_publication.py
+++ tests/test_publication.py
@@ -2,26 +2,68 @@ from libgen.publication import Publication
 
 
 def test_pub_initialization():
-    attrs = {'id': 5, 'author': 'Fernando Pessoa'}
+    attrs = {'id': 5, 'authors': 'Fernando Pessoa'}
     p = Publication(attrs)
     assert p.id == 5
-    assert p.author == 'Fernando Pessoa'
+    assert p.authors == 'Fernando Pessoa'
     assert p.title is None
 
 
 def test_pub_attributes():
-    attrs = {'id': 5, 'author': 'Fernando Pessoa'}
+    attrs = {'id': 5, 'authors': 'Fernando Pessoa'}
     p = Publication(attrs)
     assert p.attributes == attrs.keys()
 
 
 def test_pub_values():
-    attrs = {'id': 5, 'author': 'Fernando Pessoa'}
+    attrs = {'id': 5, 'authors': 'Fernando Pessoa'}
     p = Publication(attrs)
     assert list(p.values()) == list(attrs.values())
 
 
 def test_pub_repr():
-    attrs = {'id': 5, 'author': 'Fernando Pessoa'}
+    attrs = {'id': 5, 'authors': 'Fernando Pessoa'}
     p = Publication(attrs)
-    assert repr(p) == 'Publication(\'id\': 5, \'author\': Fernando Pessoa)'
+    assert repr(p) == 'Publication(\'id\': 5, \'authors\': Fernando Pessoa)'
+
+
+def test_pub_len():
+    attrs = {'id': 5, 'authors': 'Fernando Pessoa'}
+    p = Publication(attrs)
+    assert len(p) == len(attrs)
+
+
+def test_pub_filename_no_extension():
+    p = Publication({'id': 5, 'authors': 'Fernando Pessoa'})
+    assert p.filename() is None
+
+
+def test_pub_filename_extension_only():
+    p = Publication({'id': 5,
+                     'authors': 'Fernando Pessoa',
+                     'extension': 'pdf'})
+    assert p.filename().endswith('.pdf')
+
+
+def test_pub_filename_extension_and_title():
+    p = Publication({'id': 5,
+                     'title': 'Island',
+                     'extension': 'pdf'})
+    assert p.filename() == 'Island.pdf'
+
+
+def test_pub_filename_extension_title_and_year():
+    p = Publication({'id': 5,
+                     'title': 'Island',
+                     'year': 1962,
+                     'extension': 'epub'})
+    assert p.filename() == 'Island (1962).epub'
+
+
+def test_pub_filename_extension_title_authorss_and_year():
+    p = Publication({'id': 5,
+                     'title': 'Island',
+                     'authors': 'Aldous Huxley',
+                     'year': 1962,
+                     'extension': 'djvu'})
+    assert p.filename() == 'Island (1962) - Aldous Huxley.djvu'
