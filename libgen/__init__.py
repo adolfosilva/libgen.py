66d0aa707951b97bb79f6d9d8a995b49a46c3e04
Adolfo Silva <code@adolfosilva.org>
2018-02-09 09:10:33 +0000

    Add '__version__.py' with the library's metadata

--- libgen/__init__.py
+++ libgen/__init__.py
@@ -6,3 +6,8 @@ libgen
 
 Download books from 'gen.lib.rus.ec', 'libgen.io',
 'libgen.pw', 'b-ok.org' and 'bookfi.net'."""
+
+
+from .__version__ import __title__, __description__, __url__, __version__
+from .__version__ import __status__, __author__, __author_email__, __license__
+from .__version__ import __copyright__
