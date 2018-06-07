93de28cae82ec78a3c2d2af7099de175e3ed6e5a
Shadab Zafar <dufferzafar0@gmail.com>
2018-06-08 00:07:04 +0530

    Import stuff from __main__ into __init__
    Makes for a cleaner API

--- libgen/__init__.py
+++ libgen/__init__.py
@@ -11,3 +11,6 @@ Download books from 'gen.lib.rus.ec', 'libgen.io',
 from .__version__ import __title__, __description__, __url__, __version__
 from .__version__ import __status__, __author__, __author_email__, __license__
 from .__version__ import __copyright__
+
+from .__main__ import MIRRORS as AVAILABLE_MIRRORS
+from .__main__ import MirrorFinder
